3423083375bd42257760b32ab485d4bc7e39e6a9
DaniWRLam <danielwairenlam@gmail.com>
2025-11-08 14:46:15 -0800

    Update ProgCtr.sv

--- hardware/ProgCtr.sv
+++ hardware/ProgCtr.sv
@@ -1,13 +1,45 @@
 module ProgCtr(
-  input             Clk,
-                    Reset,
-					Jen,
-  input       [5:0] Jump,
+  input  logic      Clk,
+  input  logic      Reset,
+  input	 logic      Jen,
+  input  logic[1:0] cond,
+  input  logic[7:0] acc
+  input  logic[5:0] Jump,
   output logic[5:0] PC);
+);
 
+
+  logic [5:0] PCnext;
+  logic branch;
+  logic Z = (acc == 8'b0) ;
+  logic N = acc[7];
+  logic P = (~acc[7]) & (acc != 8'b0);
+
+  always_comb begin
+    case (cond)
+        2'b00: branch = 1'b1; //unconditional branch 
+        2'b01: branch = Z;    //Branch if equal to 0
+	2'b10: branch = P;    //Branch if > 
+	2'b11: branch = N;    //Branch if < 
+        default: branch = 1'b0;
+    endcase
+  end
+
+  always_comb begin
+    if (branch) 
+    	PCnext = Jump
+    else 
+	PCnext = PC + 1'b1
+    end
+  
   always_ff @(posedge Clk)
-    if(Reset) PC <= 'b0;
-	else if(Jen) PC <= Jump;
-	else      PC <= PC + 6'd1;
+    if(!Reset) begin 
+        PC <= 0;
+    end else if(Jen) begin
+        PC <= Jump;
+    end else begin
+        PC <= PC + 6'd1;
+    end
+   end
 
 endmodule
