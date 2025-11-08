82f8843da3fb6421ecf81c2e0e1f02691b0ec276
DaniWRLam <danielwairenlam@gmail.com>
2025-11-08 14:46:59 -0800

    Update RegFile.sv

--- hardware/RegFile.sv
+++ hardware/RegFile.sv
@@ -1,17 +1,25 @@
 module RegFile(
-  input      Clk,	 // clock
-             Wen,    // write enable
-  input[1:0] Ra,     // read address pointer A
-             Rb,     //                      B
-			 Wd,	 // write address pointer
-  input[7:0] Wdat,   // write data in
-  output[7:0]RdatA,	 // read data out A
-             RdatB); // read data out B
+  input  logic       Clk,	 // clock
+  input  logic       Wen,    // write enable
+  input  logic [3:0] Ra,     // read address pointer A
+  input  logic [3:0] Rb,     //                      B
+  input  logic [3:0] Wd,	 // write address pointer
+  input  logic [7:0] Wdat,   // write data in
+  output logic [7:0] RdatA,	 // read data out A
+  output logic [7:0] RdatB); // read data out B
 
-  logic[7:0] Core[4]; // reg file itself (4*8 array)
+  logic[7:0] Core[0:15]; 
 
-  always_ff @(posedge Clk)
-    if(Wen) Core[Wd] <= Wdat;
+  always_ff @(posedge Clk or posedge Rst) begin
+      if (Rst) begin
+          integer i;
+          for (i = 0; i < REG_DEPTH; i++) begin
+                Core[i] <= '0;        // clear registers on reset
+          end
+      end else if (Wen) begin
+            Core[Wd] <= Wdat;         // synchronous write
+      end
+  end
 
   assign RdatA = Core[Ra];
   assign RdatB = Core[Rb];
