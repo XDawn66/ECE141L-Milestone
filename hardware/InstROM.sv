7625fc16bac76c4f47c649d6abdfafed9dc27330
DaniWRLam <danielwairenlam@gmail.com>
2025-11-08 14:44:55 -0800

    Update InstROM.sv

--- hardware/InstROM.sv
+++ hardware/InstROM.sv
@@ -2,11 +2,11 @@ module InstROM(
   input[5:0] PC,
   output logic[8:0] mach_code);
 
-  logic[8:0] Core[64];
+  logic[8:0] Core[2**9];
 
   initial 
 	$readmemb("mach_code.txt",Core);
 
-  always_comb mach_code = Core[PC];
+  always_comb mach_code = Core[PC - 1];
 
 endmodule
