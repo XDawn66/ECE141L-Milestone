9b48effba4315e569fe023690a4193869714e345
DaniWRLam <danielwairenlam@gmail.com>
2025-12-08 14:26:55 -0800

    Update Mux.sv

--- hardware/Mux.sv
+++ hardware/Mux.sv
@@ -1,9 +1,9 @@
-module Mux(
-    input  logic [8:0] in0,   // input 0
-    input  logic [8:0] in1,   // input 1
-    input  logic       S,     // select signal
-    output logic [8:0] out    // mux output
-);
+module Mux #( parameter WIDTH = 9 ) (
+    input  logic [WIDTH-1:0] in0,   // input 0
+    input  logic [WIDTH-1:0] in1,   // input 1
+    input  logic             S,     // select signal
+    output logic [WIDTH-1:0] out    // mux output
+    );
 
     always_comb begin
         case (S)
