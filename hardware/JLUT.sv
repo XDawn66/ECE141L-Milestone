78497fb5f95a9aba2744b727edce720adfdd7e76
DaniWRLam <danielwairenlam@gmail.com>
2025-12-09 01:28:43 -0800

    Update JLUT.sv

--- hardware/JLUT.sv
+++ hardware/JLUT.sv
@@ -1,16 +1,12 @@
 module JLUT(
-    input  logic [1:0] Jptr,       // 2-bit jump type from Ctrl
-    output logic [5:0] Jump        // 6-bit jump address
+    input  logic [7:0] Jptr,      
+    output logic [15:0] Jump      
 );
 
-    always_comb begin
-        case (Jptr)
-            2'b00: Jump = 6'd10;  // J   unconditional jump to address 10
-            2'b01: Jump = 6'd12;  // JE  jump if ACC == 0
-            2'b10: Jump = 6'd8;   // JG jump if ACC > 0
-            2'b11: Jump = 6'd5;   // JL  jump if ACC < 0
-            default: Jump = 6'd0; // default
-        endcase
-    end
+  logic[15:0] Core[2**5];
+
+  initial $readmemb("jump_lut.mem", Core);
+  
+  always_comb Jump = Core[Jptr];
 
 endmodule
